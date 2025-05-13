1c9e5c413aea33a67489cbada8d2c6cadd956d92
SlothSimian <16jkumar@gmail.com>
2025-05-13 10:54:57 +0530

    All files added

---
+++ Assignment/assignment3.js
@@ -0,0 +1,9 @@
+function bw() {
+    // window.alert("Color Change");
+    document.getElementById("i1").src="/images/download1.jpg";
+}
+
+function clr() {
+    // alert();
+    document.getElementById("i1").src="/images/download.jpg";
+}
