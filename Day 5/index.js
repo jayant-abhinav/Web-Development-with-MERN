f1036e600eb3d43d51668871c57251da4325e795
SlothSimian <16jkumar@gmail.com>
2025-05-20 10:59:07 +0530

    File added

--- Day 5/index.js
+++ Day 5/index.js
@@ -2,15 +2,15 @@ function p(){
         let h = window.innerHeight;
         let w = window.innerWidth;
 
-        confirm(`height of windo =${h}and width=${w}`);
+        confirm(`height=${h} and width=${w}`);
 }
 let a;
 function o(){
      a = window.open("","",width=200,height=100);
 }
-// function o1(){ 
-//     let a = window.open("https://getbootstrap.com/docs/5.3/getting-started/introduction/","_blank",width=200,height=100);
-// }
+function o1(){ 
+    let a = window.open("https://getbootstrap.com/docs/5.3/getting-started/introduction/","_blank",width=200,height=100);
+}
 function c(){
         a.close();
 }
